45eabdec687bf9eba86b897890c09d6ca5f9f1d2
Tarun Tehri <tehritarun@gmail.com>
2024-08-13 18:38:30 +0530

    updated code to working state

--- main.py
+++ main.py
@@ -1,35 +1,52 @@
+import json
+import os
+
+
 def load_config(path):
-    with open(path,'r') as f:
+    with open(path, 'r') as f:
         return json.load(f)
+
+
 def create_session(session_name, windows: list):
-    print(f"creating session (session_name}")
-    os.system(f"tmux new-session -d -s (session _name}")
-    for index, win in enumerate(windows) :
-        create_window(session_name, index ==0, win)
+    print(f"creating session {session_name}")
+    os.system(f"tmux new-session -d -s {session_name}")
+    for index, win in enumerate(windows):
+        create_window(session_name, index == 0, win)
+
+
 def create_window(session_name: str, firstwindow: bool, window: dict):
-    session_name = f"-t {session_name}."
+    session_name = f"-t {session_name}"
     if not firstwindow:
-        print(f"creating window: {window ['windowName ']}")
+        print(f"creating window: {window ['windowName']}")
         # Creating new window
-        print(f'tmux new-window {session_name} -n {window ["windowName" ]}')
-        os.system(f'tmux new-window {session _name} -n {window ["windowName" 1}')
+        print(f'tmux new-window {session_name} -n {window ["windowName"]}')
+        os.system(f'tmux new-window {session_name} -n {window ["windowName"]}')
     print(f"renaming window: {window[ 'windowName' ]}")
     # Renaming window
-    os.system(f'tmux rename-window {window ["windowName" ]} ') os. system(
-    f'tmux send-keys {session_name} "(window["panes"] [0] ["command" ] }" C-m') for pane in window["panes"] [1:]:
-    print(f"creating pane {panel'orientation'1}")
-    # setting up pane
-    if panel["size"]:
-        option = f'-{panel 'orientation'] [0]}l {pane ["size"]}'
-        os. system(f"tmux split-window {option} {session _name}")
-        os system (f'tmux send-keys {session_name} "{pane ["command" ]}" C-m')
+    os.system(f'tmux rename-window {window ["windowName" ]}')
+    os. system(
+        f'tmux send-keys {session_name} "{window["panes"][0]["command"]}" C-m')
+    for pane in window["panes"][1:]:
+        print(f"creating pane {pane['orientation']}")
+        # setting up pane
+        if pane["size"]:
+            option = f"-{pane['orientation'][0]}l {pane['size']}"
+            os.system(f"tmux split-window {option} {session_name}")
+            os.system(
+                f'tmux send-keys {session_name} "{pane ["command"]}" C-m')
+
+
 def main():
-    layouts = load_config(".")
-    layout_names = "\n" join(In for n in layouts. items ()])
+    layouts = load_config("layouts.json")
+    layout_names = "\n".join([n for n, _ in layouts.items()])
     os.system(f"echo '{layout_names}'| fzf > selectedoption")
-    with open ("selectedoption","r") as f:
+    with open("selectedoption", "r") as f:
         option = f.readlines()[0]
-    layout = layouts[option].strip()
+    layout = layouts[option.strip()]
     session_name = os.path.realpath(os.curdir).split('/')[-1]
-    create_session(session_name, layout ["windows"])
+    create_session(session_name, layout["windows"])
     os.system(f"tmux attach-session -t {session_name}")
+
+
+if __name__ == "__main__":
+    main()
