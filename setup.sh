fdb5c4d5085f24596c89a6975d50afd501cccc71
Tarun Tehri <tehritarun@gmail.com>
2025-10-09 22:00:15 +0530

    verbose added in install script

--- setup.sh
+++ setup.sh
@@ -1,17 +1,19 @@
 # check and create folder if not present
 configdir="$HOME/.config/tmux-sess"
 if [ -d "$configdir" ]; then
-    echo "configuration folder is already present"
+    echo "Configuration folder is already present"
 else
     mkdir "$configdir"
+    echo "Configuration folder created.."
 fi
 
 # check and crete configuration if not present
 if [ -f "$configdir"/tmux-sess.json ]; then
-    echo "configuration is already present"
+    echo "Configuration is already present"
 else
     echo "./layouts.json on $configdir/tmux-sess.json"
     cp ./layouts.json "$configdir"/tmux-sess.json
+    echo "Example configuration created"
 fi
 
 # check and create zshrc entry is not present
@@ -21,6 +23,7 @@ echo "$sessEntry"
 if [ -z "$sessEntry" ]; then
     projectdir=$PWD/main.py
     echo "alias tt='python3 $projectdir'" >> "$HOME"/.zshrc
+    echo "Entry added in zshrc"
 else
     echo 'zshrc entry is already available'
 fi
